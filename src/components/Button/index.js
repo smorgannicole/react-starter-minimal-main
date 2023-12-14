3a3e36c48ace125e485c82c32cb95cc68bc77bb2
Morgan <smorgannicole@gmail.com>
2023-12-14 11:52:44 -0600

    button event

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,9 @@
 const Button = (props) => {
+    const handleButtonClick = (e) => {
+        alert("I've been clicked!")
+    }
     return (
-        <button 
+        <button onClick={handleButtonClick}
             className={
                 props.ghost 
                 ? `btn btn-outline-${props.color ? props.color: "primary"} d-flex justify-content-center align-items-center gap-3` 
